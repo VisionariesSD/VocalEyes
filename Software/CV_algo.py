9b47122fad71058c04054b82fd796875d113e84c
Jason_Lim <jasonbernardeudela.lim@gmail.com>
2021-06-13 20:20:59 -0500

    Added CV portion of the algorithm w/ test images

--- Software/CV_algo.py
+++ Software/CV_algo.py
@@ -1,8 +1,58 @@
-from PIL import Image
+import convert_to_speech
+import cv2
+import pytesseract
+import numpy as np
 
-#img = Image.open('/home/pi/Pictures/test.jpg')
+#TODO: NEED TO WORK ON THE SETTINGS REGARDING LANGUAGE AND SPEED
 
-#img.show()
+pytesseract.pytesseract.tesseract_cmd = 'C:\\Program Files\\Tesseract-OCR\\tesseract.exe' #!FOR TESTING SHOULD BE UNNECESSARY WITH LINUX
 
-print ("X")
+#----------------------------------------------------------------Read in image----------------------------------------------------------------
+img = cv2.imread('/home/pi/Pictures/test.jpg')
+#img = cv2.imread('skew.jpg')  #!FOR TESTING
+
+#-----------------------------------------------------Perform preprocessing of the image------------------------------------------------------
+
+#Grayscale conversion
+grayscale = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+#Gaussian blur conversion
+gaussian = cv2.GaussianBlur(grayscale, (1,1), 0)
+
+#Threshold conversion
+ret, threshold = cv2.threshold(gaussian,0,255,cv2.THRESH_BINARY_INV+cv2.THRESH_OTSU)
+
+#Correcting any possible skew in the image
+coords = np.column_stack(np.where(threshold > 0))
+angle = cv2.minAreaRect(coords)[-1]
+if angle < -45:
+    angle = -(90 + angle)
+else:
+    angle = -angle
+
+(h, w) = threshold.shape[:2]
+center = (w // 2, h // 2)
+M = cv2.getRotationMatrix2D(center, angle, 1.0)
+rotated = cv2.warpAffine(threshold, M, (w, h), flags=cv2.INTER_CUBIC, borderMode=cv2.BORDER_REPLICATE)
+
+#!FOR TESTING
+#cv2.imshow('test.png',rotated)
+#cv2.waitKey(0)
+#cv2.destroyAllWindows()
+
+#---------------------------------------------------------Convert the image to text---------------------------------------------------------
+custom_config = r'--oem 3 --psm 6'
+text = pytesseract.image_to_string(rotated, lang=None, config=custom_config) #TODO: Need to fix the language part
+
+#print(text) #!FOR TESTING
+
+#---------------------------------------------------------Convert the text to an mp3--------------------------------------------------------
+convert_to_speech.txt_to_mp3(text, 'en')
+
+
+#-------------------------------------------------------------Play the audio----------------------------------------------------------------
+
+
+
+
 #subprocess.call('echo done_showing', shell = True)
--- Software/convert_to_speech.py
+++ Software/convert_to_speech.py
@@ -1,27 +1,15 @@
 from gtts import gTTS
 import requests
 
-def txt_to_mp3(file_name, language):
+def txt_to_mp3(txt, language):
 
 	# need this disabled for the api to transmit data to google 
 	requests.packages.urllib3.disable_warnings() 
-
-	# open file containing text
-	fp = open(file_name, 'r')
-	
-	# read each line of text into a list
-	txt = fp.readlines()
-
-	# string for text without new lines characters
-	remove_newline = ""
-	
-	# loop through removing new lines and making string for audio output
-	for temp0 in txt:
-
-		remove_newline = remove_newline + temp0
 		
 	# pass string, language, and speed of speech to api for conversion to mp3
-	tts = gTTS(text=remove_newline, lang=language, slow=False)
+	tts = gTTS(text=txt, lang=language, slow=False) #TODO: Need to fix the language part
 	
 	# save mp3 file in current directory
-	tts.save('speech_output.mp3')
+	tts.save('/home/pi/Pictures/speech_output.mp3')
+
+	#tts.save('speech_output.mp3') #!FOR TESTING
