474bd93c3336b303a0ea49f0c61713d9f9bad193
JasonBernard_Lim <pokemonmaster967@gmail.com>
2021-07-08 14:40:35 -0500

    Added extra notes determining sections needed

--- Software/CV_algo.py
+++ Software/CV_algo.py
@@ -36,6 +36,10 @@ def VolumeDown(channel):
     time.sleep(1.5) #For debouncing
     
 #TODO: NEED TO WORK ON THE SETTINGS REGARDING LANGUAGE AND SPEED
+#SETTINGS HANDLING SECTION
+language = 'en'
+
+#TODO: NEED TO IMPLEMENT BLUETOOTH WIFI SIGNAL RETRIEVAL
 
 #pytesseract.pytesseract.tesseract_cmd = 'C:\\Program Files\\Tesseract-OCR\\tesseract.exe' #!FOR TESTING SHOULD BE UNNECESSARY WITH LINUX
 
@@ -68,23 +72,25 @@ grayscale = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 gaussian = cv2.GaussianBlur(grayscale, (1,1), 0)
 
 #Threshold conversion
+#?: Try removing THRESH_OTSU
 ret, threshold = cv2.threshold(gaussian,0,255,cv2.THRESH_BINARY_INV+cv2.THRESH_OTSU)
 
+#!Possibility of being removed
 #Correcting any possible skew in the image
-coords = np.column_stack(np.where(threshold > 0))
-angle = cv2.minAreaRect(coords)[-1]
-if angle < -45:
-    angle = -(90 + angle)
-else:
-    angle = -angle
+# coords = np.column_stack(np.where(threshold > 0))
+# angle = cv2.minAreaRect(coords)[-1]
+# if angle < -45:
+#     angle = -(90 + angle)
+# else:
+#     angle = -angle
 
-(h, w) = threshold.shape[:2]
-center = (w // 2, h // 2)
-M = cv2.getRotationMatrix2D(center, angle, 1.0)
-rotated = cv2.warpAffine(threshold, M, (w, h), flags=cv2.INTER_CUBIC, borderMode=cv2.BORDER_REPLICATE)
+# (h, w) = threshold.shape[:2]
+# center = (w // 2, h // 2)
+# M = cv2.getRotationMatrix2D(center, angle, 1.0)
+# rotated = cv2.warpAffine(threshold, M, (w, h), flags=cv2.INTER_CUBIC, borderMode=cv2.BORDER_REPLICATE)
 
-#ERROR ON ROTATING SO IGNORING THE ROTATION FOR NOW
-rotated = img
+# #ERROR ON ROTATING SO IGNORING THE ROTATION FOR NOW
+# rotated = img
 
 #!FOR TESTING
 #cv2.imshow('test.png',rotated)
@@ -93,12 +99,12 @@ rotated = img
 
 #---------------------------------------------------------Convert the image to text---------------------------------------------------------
 custom_config = r'--oem 3 --psm 6'
-text = pytesseract.image_to_string(rotated, lang=None, config=custom_config) #TODO: Need to fix the language part
+text = pytesseract.image_to_string(threshold, lang=None, config=custom_config) #TODO: Need to fix the language part
 
 #print(text) #!FOR TESTING
 
 #---------------------------------------------------------Convert the text to an mp3--------------------------------------------------------
-convert_to_speech.txt_to_mp3(text, 'en')
+convert_to_speech.txt_to_mp3(text, language)
 
 
 #-------------------------------------------------------------Play the audio----------------------------------------------------------------
